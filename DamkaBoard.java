7be864b37cca9623c1a624f39eefbf8f1248c43f
OmerKirshner <154836683+OmerKirshner@users.noreply.github.com>
2024-03-01 17:39:29 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,16 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt(args[0]);
+   		for (int i = 1 ; i <= n ; i++) {
+   			for (int j = 1 ; j <= n ; j++) {
+   				if (i % 2 == 1) { 
+   					System.out.print("* ");
+   				} else {
+   					System.out.print(" *");
+   				}
+   			}
+   			System.out.println();	
+		}
 	}
 }
